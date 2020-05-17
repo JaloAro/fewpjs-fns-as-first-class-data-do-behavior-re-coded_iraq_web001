3f286b66497cfc049eb3ec94446aa03c08e7b6dc
jalal Arif <jalalharfjalal@gmail.com>
2020-05-17 13:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Given Code, don't edit */
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
@@ -8,4 +6,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeInString){
+
+const parsed = parseInt(timeInString);
+if(parsed<12){
+
+  return "Good Morning"
+}
+if(parsed>12&&parsed<17){
+
+  return "Good Afternoon"
+}
+if(parsed>17){
+
+  return "Good Evening"
+}
+}
 /* Write your implementation of displayMessage() */
+ function displayMessage(message){
+
+  let greetingMessage= document.getElementById('greeting');
+  greetingMessage.innerText = message;
+  console.log(message);
+}
